962f96238d9ad63a1a69497d1ff3df2e706b76a9
Tim Van Dyck <tim.van.dyck@davinsi.com>
2025-12-31 17:33:35 +0100

    feat: Remove unused lxcs

--- lxc/inventory_lxc.tf
+++ lxc/inventory_lxc.tf
@@ -69,32 +69,10 @@ locals {
         }
       }
     },
-    "lxc-podman" = {
-      config = {
-        distro  = "debian"
-        memory  = "2048"
-        cores   = "4"
-        tags    = "podman;tailscale"
-        nesting = true
-        keyctl  = true
-      }
-    },
     "lxc-kuma" = {
       config = {
         distro = "debian12"
         tags   = "kuma;tailscale"
-      }
-    },
-    "lxc-syslog" = {
-      config = {
-        distro = "debian12"
-        tags   = "syslogng;tailscale"
-        #   splunk_inputs_monitor_logfile = {
-        #     "syslog_remote" = {
-        #       sourcetype = "syslog"
-        #       index      = "syslog"
-        #     }
-        # }
       }
     },
     "lxc-tailscale" = {
@@ -103,12 +81,6 @@ locals {
         distro   = "ubuntu"
         cores    = "4"
         tags     = "tailscale"
-      }
-    },
-    "lxc-telegraf" = {
-      config = {
-        distro = "debian12"
-        tags   = "tailscale;telegraf"
       }
     },
     "lxc-pihole2" = {
@@ -185,27 +157,6 @@ locals {
         }
       },
     }
-    "lxc-timemachine" = {
-      config = {
-        hostname = "lxc-timemachine"
-        distro   = "debian12"
-        tags     = "samba;tailscale;timemachine"
-        mounts = {
-          "timemachine" = {
-            "volume" = "/mnt/samsung_pm/binds/samba_data_tm"
-            "mp"     = "/timemachine"
-            "size"   = "600G"
-          },
-        }
-        "networks" = {
-          "eth0" = {
-            "ip"  = "192.168.2.15/24"
-            "gw"  = "192.168.2.1"
-            "tag" = "2"
-          }
-        }
-      },
-    }
     "lxc-qbit" = {
       config = {
         hostname = "lxc-qbit"
@@ -254,21 +205,5 @@ locals {
         admin = {}
       },
     },
-    "lxc-pangolin" = {
-      config = {
-        hostname = "lxc-pangolin"
-        distro   = "debian12"
-        tags     = "pangolin;ssh;tailscale"
-        memory   = "4096"
-        nesting  = true
-        cores    = "2"
-        "networks" = {
-          "eth0" = {
-            ip  = "dhcp"
-            tag = 2
-        } }
-      },
-    },
-
   }
 }
