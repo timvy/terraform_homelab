f1e8d8bd3e7d5e1490b9886c5226c38bd6fd9c67
Tim Van Dyck <tim.van.dyck@davinsi.com>
2026-01-02 12:08:45 +0100

    feat: git ignore backend

--- lxc/provider.tf
+++ lxc/provider.tf
@@ -17,15 +17,6 @@ terraform {
       source = "tailscale/tailscale"
     }
   }
-  backend "s3" {
-    bucket                      = "tofu-backend"
-    key                         = "terraform.tfstate"
-    # key                         = "env:/lxc/terraform.tfstate"
-    region                      = "main" # this is required, but will be skipped!
-    skip_credentials_validation = true   # this will skip AWS related validation
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-  }
 }
 
 provider "bitwarden" {
