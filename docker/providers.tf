5a53795efd27630aab650d720134fc9fca5b2725
timvy <remark-brick-april@duck.com>
2026-02-11 18:30:01 +0000

    feat: Move backend to separate file

--- docker/providers.tf
+++ docker/providers.tf
@@ -16,16 +16,6 @@ terraform {
       source = "tailscale/tailscale"
     }
   }
-  backend "s3" {
-    bucket = "tofu-backend"
-    # Semaphore puts the state file in environment subfolders /env:/docker
-    key                         = "env:/docker/terraform.tfstate"
-    # key                         = "terraform.tfstate"
-    region                      = "main" # this is required, but will be skipped!
-    skip_credentials_validation = true   # this will skip AWS related validation
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-  }
 }
 
 provider "random" {}
