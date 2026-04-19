038b7a3812a1cf0cd74ee1f52b791c47bca19ec8
timvy <remark-brick-april@duck.com>
2026-04-19 16:54:33 +0200

    feat: add backend

--- docker/backend.tf
+++ docker/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "tofu-backend"
     # Semaphore puts the state file in environment subfolders /env:/docker
-    # key                         = "env:/docker/terraform.tfstate"
+    # key = "env:/docker/terraform.tfstate"
     key                         = "terraform.tfstate"
     region                      = "main" # this is required, but will be skipped!
     skip_credentials_validation = true   # this will skip AWS related validation
